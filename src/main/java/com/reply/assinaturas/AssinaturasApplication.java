e23a304f8df8546ba614fada0d1e265c981bff05
luizfernandodag <luizfernandodag@gmail.com>
2025-10-19 10:01:23 -0300

    ini projeto

---
+++ src/main/java/com/reply/assinaturas/AssinaturasApplication.java
@@ -0,0 +1,13 @@
+package com.reply.assinaturas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssinaturasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssinaturasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reply/assinaturas/AssinaturasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reply.assinaturas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssinaturasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
